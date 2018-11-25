0d587049976aec5b98e9cd87da86f8301be17cf4
Jerry Gerou <jerrysgerou@gmail.com>
2018-11-25 15:15:35 -0600

    Logging working for other buttons

--- app.js
+++ app.js
@@ -10,52 +10,82 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
+
   methods: {
     startGame: function() {
       this.gameIsRunning = true;
       this.playerHealth = 100;
-      this. monsterHealth = 100;
+      this.monsterHealth = 100;
+      this.turns = [];
     },
+
     attack: function() {
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      });
       if (this.checkWin()) {
         return;
       }
 
       this.monsterAttacks();
     },
+
     specialAttack: function() {
-      this.monsterHealth -= this.calculateDamage(10, 20);
+      var damage = this.calculateDamage(10, 20)
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster HARD for ' + damage
+      });
       if (this.checkWin()) {
         return;
       }
       this.monsterAttacks();
     },
+
     monsterAttacks: function() {
-      this.playerHealth -= this.calculateDamage(5, 12);
+      var damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits Player for ' + damage
+      });
       this.checkWin();
     },
+
     heal: function() {
       if (this.playerHealth <= 90) {
         this.playerHealth +=10;
       } else {
         this.playerHealth = 100;
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for 10'
+      });
       this.monsterAttacks();
     },
+
     giveUp: function() {
       this.gameIsRunning = false;
     },
+
     // Damage needs to be a random number with a max and min possible
     calculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
+
     checkWin: function() {
       if (this.monsterHealth <= 0) {
         if (confirm('You won! New Game?')) {
           this.startGame();
+          this.turns = []
         } else {
           this.gameIsRunning = false;
         }
