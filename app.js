18dc0de0d4e8d488e601e27a96bf916ad8ea96b4
Jerry Gerou <jerrysgerou@gmail.com>
2018-11-25 14:00:22 -0600

    Attack and end of game working

--- app.js
+++ app.js
@@ -20,28 +20,15 @@ new Vue({
     },
     attack: function() {
       // Damage needs to be a random number with a max and min possible
-      // Give monster more damage than player
-      var max = 10;
-      var min = 3;
-      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      var damage = this.calculateDamage(3, 10);
       this.monsterHealth -= damage;
 
-      if (this.monsterHealth <= 0) {
-        alert('You won!');
-        this.gameIsRunning = false;
-        return;
-      }
+      this.checkWin();
 
-      max = 12;
-      min = 3;
-      damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      damage = this.calculateDamage(3, 12);
       this.playerHealth -= damage;
 
-      if (this.playerHealth <= 0) {
-        alert('You lost!');
-        this.gameIsRunning = false;
-        return;
-      }
+      this.checkWin();
     },
     specialAttack: function() {
 
@@ -51,6 +38,27 @@ new Vue({
     },
     giveUp: function() {
 
+    },
+    calculateDamage: function(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin: function() {
+      if (this.monsterHealth <= 0) {
+        if (confirm('You won! New Game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm('You lost! New Game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
     }
   }
 });
